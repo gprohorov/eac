62355dda521371781b622f70c751d8f337b57dc4
george2 <yuramail222@gmail.com>
2017-02-21 17:39:09 +0200

    Skeleton

---
+++ src/main/java/pro/spbootangular/EacApplication.java
@@ -0,0 +1,12 @@
+package pro.spbootangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EacApplication.class, args);
+	}
+}
